a2d29626ff1693290ef7f03234b3334a146e72a3
Chris Jhone <cjlozada9@gmail.com>
2026-04-03 02:47:46 +0800

    update url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://organological-unenticeable-lamar.ngrok-free.dev/',
+  apiUrl: 'https://quiz-upload-api-asfja5gzcjf8bab9.southeastasia-01.azurewebsites.net/',
   // SignalR hub URL for realtime monitor (adjust server route if needed)
   signalRHubUrl: 'https://organological-unenticeable-lamar.ngrok-free.dev/hub/monitor'
 };
