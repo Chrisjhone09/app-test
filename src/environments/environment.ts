db6a8a2b3e4d2db25dc2952e491bc929a1c0d114
Chris Jhone Radana <cjlozada9@gmail.com>
2026-04-28 00:14:29 +0800

    final na jud

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://organological-unenticeable-lamar.ngrok-free.dev/',
+  apiUrl: 'https://ai-quiz-app-bcfrc4geegcxhtgf.canadacentral-01.azurewebsites.net/',
   // SignalR hub URL for realtime monitor (adjust server route if needed)
   signalRHubUrl: 'https://organological-unenticeable-lamar.ngrok-free.dev/hub/monitor'
 };
