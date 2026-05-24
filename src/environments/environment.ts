4ec9f30a80366989beb94e18d5779b280dff41db
Chris Jhone Radana <cjlozada9@gmail.com>
2026-05-24 19:01:22 +0800

    change api env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ai-quiz-app-bcfrc4geegcxhtgf.canadacentral-01.azurewebsites.net/',
+  apiUrl: 'https://cdk-attendance.runasp.net/',
   // SignalR hub URL for realtime monitor (adjust server route if needed)
   signalRHubUrl: 'https://organological-unenticeable-lamar.ngrok-free.dev/hub/monitor'
 };
